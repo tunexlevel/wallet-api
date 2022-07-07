4eb6180d3307da7c8474bb1e8b82c34d5e90b235
Opesemowo Babatunde <tunexlevel8@gmail.com>
2022-07-07 23:45:08 +0100

    fix txn history

--- src/controllers/transactionController.ts
+++ src/controllers/transactionController.ts
@@ -56,17 +56,17 @@ class transactionController extends userController {
 
             const session_id = moment().format('x').toString() + check?.userId;
 
-            const transaction_date = moment().format('YYYY-MM-DD HH:mm:ss');
+            this.transaction_date = moment().format('YYYY-MM-DD HH:mm:ss');
 
 
-            if (check.status !== 200 || !check?.userId 
-                || !check?.sBalance 
-                || !check?.rBalance 
+            if (check.status !== 200 || !check?.userId
+                || !check?.sBalance
+                || !check?.rBalance
                 || !check?.rName
                 || !check?.sName
             ) {
-                if(check?.sName && check?.sBalance && check?.userId){
-                    this.failedTxn(check.sName, check.sBalance, session_id, check.userId )
+                if (check?.sName && check?.sBalance && check?.userId) {
+                    this.failedTxn(check.sName, check.sBalance, session_id, check.userId)
                 }
                 return check;
             }
@@ -79,7 +79,7 @@ class transactionController extends userController {
                 debit_wallet_id: this.sender_wallet_id,
                 user_id: check.userId,
                 status: "Successful",
-                transaction_date: transaction_date,
+                transaction_date: this.transaction_date,
                 created_at: new Date()
             }, { transaction: t })
 
@@ -106,7 +106,7 @@ class transactionController extends userController {
                 credit_wallet_id: this.receiver_wallet_id,
                 debit_wallet_id: this.sender_wallet_id,
                 amount: this.amount,
-                transaction_date: transaction_date,
+                transaction_date: this.transaction_date,
                 pre_balance: check.sBalance,
                 post_balance: check.sBalance - this.amount
             }, { transaction: t })
@@ -122,14 +122,14 @@ class transactionController extends userController {
                 credit_wallet_id: this.receiver_wallet_id,
                 debit_wallet_id: this.sender_wallet_id,
                 amount: this.amount,
-                transaction_date: transaction_date,
+                transaction_date: this.transaction_date,
                 pre_balance: check.rBalance,
                 post_balance: check.rBalance + this.amount
             }, { transaction: t })
 
             const summary = {
                 session_id,
-                transaction_date,
+                transaction_date: this.transaction_date,
                 sender: check.sName,
                 receiver: check.rName,
                 amount: this.amount
@@ -177,7 +177,7 @@ class transactionController extends userController {
             }
             else if ((sendUser.balance - this.amount) < 0) {
 
-                return { status: 400, message: "Insuffucient fund" };
+                return { status: 400, message: "Insuffucient fund", userId: sendUser.user_id, sName: sendUser.name, sBalance: sendUser.balance };
             }
 
             const receiveUser = await this.getUserByWalletId(this.receiver_wallet_id);
@@ -194,54 +194,65 @@ class transactionController extends userController {
     }
 
     async txnHistory(id: number) {
-        const user = await Wallet.findOne({ where: {[Op.or]: [{ wallet_id: id }, {user_id: id}]} })
-        if(!user?.user_id){
-            return {status: 400, message: "User id not found"}
-        }
-        const txnHistory = await HistoryTransaction.findAll({attributes:['amount', 'type', 'transaction_date', 'user_id', 'recipient_id', 'recipient_name', 'status'], where: { user_id: id } });
+        try {
+            const user = await Wallet.findOne({ where: { [Op.or]: [{ wallet_id: id }, { user_id: id }] } })
+            if (!user?.user_id) {
+                return { status: 400, message: "User id not found" }
+            }
+            const txnHistory = await HistoryTransaction.findAll({ attributes: ['amount', 'type', 'transaction_date', 'user_id', 'recipient_id', 'recipient_name', 'status'], where: { user_id: id }, order: [['id', 'DESC']] });
 
-        return {status: 200, history:txnHistory}
+            return { status: 200, history: txnHistory }
+        }
+        catch (e) {
+            return { status: 200, message:  "Failed to get history", reason: e}
+        }
     }
 
     async txnJobs() {
-        const txns = await Batch.findAll({attributes:
-            ['id', 'user_id', 'type', 'flag', 'session_id',
-            'credit_wallet_id', 'debit_wallet_id', 
-            'amount', 'transaction_date', 'status', 'created_at']
+        const txns = await Batch.findAll({
+            attributes:
+                ['id', 'user_id', 'type', 'flag', 'session_id',
+                    'credit_wallet_id', 'debit_wallet_id',
+                    'amount', 'transaction_date', 'status', 'created_at']
         });
-        if(txns.length < 1){
-            return {status: 200, message: "No jobs found yet"}
+        if (txns.length < 1) {
+            return { status: 200, message: "No jobs found yet" }
         }
 
-        return {status: 200, lists: txns}
+        return { status: 200, lists: txns }
     }
 
-    async failedTxn(name:string, balance:number, session_id: string, user_id: number) {
-        await WalletTransaction.create({
-            amount: this.amount,
-            session_id: session_id,
-            credit_wallet_id: this.receiver_wallet_id,
-            debit_wallet_id: this.sender_wallet_id,
-            user_id: user_id,
-            status: "Failed",
-            transaction_date: this.transaction_date,
-            created_at: new Date()
-        })
+    async failedTxn(name: string, balance: number, session_id: string, user_id: number) {
+        try {
+            await WalletTransaction.create({
+                amount: this.amount,
+                session_id: session_id,
+                credit_wallet_id: this.receiver_wallet_id,
+                debit_wallet_id: this.sender_wallet_id,
+                user_id: user_id,
+                status: "Failed",
+                transaction_date: this.transaction_date,
+                created_at: new Date()
+            })
 
-        await HistoryTransaction.create({
-            session_id: session_id,
-            user_id: this.sender_wallet_id,
-            recipient_id: this.receiver_wallet_id,
-            recipient_name: name,
-            status: "Failed",
-            type: 'Debit',
-            credit_wallet_id: this.receiver_wallet_id,
-            debit_wallet_id: this.sender_wallet_id,
-            amount: this.amount,
-            transaction_date: this.transaction_date,
-            pre_balance: balance,
-            post_balance: balance
-        })
+            await HistoryTransaction.create({
+                session_id: session_id,
+                user_id: this.sender_wallet_id,
+                recipient_id: this.receiver_wallet_id,
+                recipient_name: name,
+                status: "Failed",
+                type: 'Debit',
+                credit_wallet_id: this.receiver_wallet_id,
+                debit_wallet_id: this.sender_wallet_id,
+                amount: this.amount,
+                transaction_date: this.transaction_date,
+                pre_balance: balance,
+                post_balance: balance
+            })
+        }
+        catch (e) {
+            console.log(e)
+        }
     }
 
 }
