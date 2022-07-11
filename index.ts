583b5635d619e41de4e471471e745dca54e386ff
Opesemowo Babatunde <tunexlevel8@gmail.com>
2022-07-11 20:10:17 +0100

    removed unused files

--- index.ts
+++ index.ts
@@ -1,49 +1,14 @@
-import http from "http";
-import express from "express";
-import bodyparser from "body-parser";
-import cors from "cors";
-import swaggerUi from "swagger-ui-express";
-import Yaml from "yamljs";
 import dotenv from "dotenv";
-import routes from "./src/routes/index"
 import startJobs from "./src/cron/index"
+import moment from "moment";
+
 
 dotenv.config()
 
-const app = express();
+console.log(moment().format('YYYY-MM-DD HH:mm:ss'),  'SERVIC JOB RUNNING')
 
-let server = http.createServer(app);
-
-let swaggerDocument
-
-if(process.env.NODE_ENV === "production"){
-    swaggerDocument = Yaml.load('./doc/production.yaml');
-}
-else{
-    swaggerDocument = Yaml.load('./doc/development.yaml');
-}
+startJobs();
 
 
-console.log("OK. Deployment mode set to ", process.env.PORT);
-
-server.listen(process.env.PORT);
-
-console.log("App running on this port ", process.env.PORT);
-
-//startJobs();
-
-app.use(cors());
-
-app.use(bodyparser.json({ limit: "50mb" }));
-
-app.use(bodyparser.urlencoded({ limit: "50mb", parameterLimit: 100000, extended: true }));
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.use('/v1', routes);
-
-app.get("*", function (req, res) {
-    res.send("************* WALLET API V1.O.0 **************");
-});
 
 
